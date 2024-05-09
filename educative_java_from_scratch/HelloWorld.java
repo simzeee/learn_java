752d3d5aed9556861de946eeb4ecd932f4b398d5
simzeee <jwsims816@gmail.com>
2024-05-09 00:23:04 -0400

    add Java files

---
+++ educative_java_from_scratch/HelloWorld.java
@@ -0,0 +1,8 @@
+package educative_java_from_scratch;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ educative_java_from_scratch/MyJavaApp.java
@@ -0,0 +1,8 @@
+package educative_java_from_scratch;
+
+public class MyJavaApp {
+  public static void main(String[] args) {
+    System.out.println(Math.round(3.14159));
+    System.out.println(Math.pow(2, 3));
+  }
+}
