3fd7728a79dc6bded8044b72861415f873a139d0
simzeee <jwsims816@gmail.com>
2024-05-10 13:22:21 -0400

    finish basic quiz

--- educative_java_from_scratch/MyJavaApp.java
+++ educative_java_from_scratch/MyJavaApp.java
@@ -1,8 +1,37 @@
-package educative_java_from_scratch;
+import java.util.Scanner;
 
 public class MyJavaApp {
   public static void main(String[] args) {
-    System.out.println(Math.round(3.14159));
-    System.out.println(Math.pow(2, 3));
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Please enter your name: ");
+    String userName = scan.nextLine();
+
+    String welcomeMessage = "Hello, " + userName + "! Welcome to the math quiz.";
+    System.out.println(welcomeMessage);
+
+    String question = "What is 3+2? ";
+    System.out.print(question);
+    String userAnswer = scan.nextLine();
+
+    question = "What is 6/2? ";
+    System.out.print(question);
+    userAnswer = scan.nextLine();
+
+    question = "What is 3*5? ";
+    System.out.print(question);
+    userAnswer = scan.nextLine();
+
+    question = "What is 6-2? ";
+    System.out.print(question);
+    userAnswer = scan.nextLine();
+
+    question = "What is 15/5? ";
+    System.out.print(question);
+    userAnswer = scan.nextLine();
+
+    // Make a customized farewell message and print it.
+    String farewellMessage = "Thanks for giving the quiz, " + userName + ". You will get your result soon.";
+    System.out.println(farewellMessage);
+
   }
 }
