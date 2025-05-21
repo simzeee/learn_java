030fe73a64f908ea4f14e5055b275e92c3ce17bf
simzeee <johnsims.dev@gmail.com>
2025-05-21 15:42:12 -0400

    add total heartbeats

--- CS_110/2_1_2/Lesson_3.java
+++ CS_110/2_1_2/Lesson_3.java
@@ -21,4 +21,27 @@ public class Lesson_3 {
 
     System.out.println("Average weight: " + averageWeight);
   }
+
+  public static void total_heart_beats(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    int userAgeYears;
+    int userAgeDays;
+    int userAgeMinutes;
+    int totalHeartbeats;
+    int avgBeatsPerMinute = 72;
+
+    System.out.print("Enter your age in years: ");
+    userAgeYears = scnr.nextInt();
+
+    userAgeDays = userAgeYears * 365; // Calculate days without leap years
+    userAgeDays = userAgeDays + (userAgeYears / 4); // Add days for leap years
+
+    System.out.println("You are " + userAgeDays + " days old.");
+
+    userAgeMinutes = userAgeDays * 24 * 60; // 24 hours/day, 60 minutes/hour
+    System.out.println("You are " + userAgeMinutes + " minutes old.");
+
+    totalHeartbeats = userAgeMinutes * avgBeatsPerMinute;
+    System.out.println("Your heart has beat " + totalHeartbeats + " times.");
+  }
 }
