e82305151fa850c841e096bc2bf09d0cc763e3ee
simzeee <jwsims816@gmail.com>
2025-05-23 23:14:25 -0400

    add average family size and learn type casting

--- CS_110/2_1_2/Lesson_3.java
+++ CS_110/2_1_2/Lesson_3.java
@@ -1,107 +1,126 @@
+
 import java.util.Scanner;
 
 public class Lesson_3 {
-  public static void averageWeight(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    int cheetahPounds = 65;
-    int goatPounds = 75;
-    int penguinPounds = 72;
-    int numCheetahs;
-    int numGoats;
-    int numPenguins;
-    int averageWeight;
-    int sumWeight;
 
-    numCheetahs = scnr.nextInt();
-    numGoats = scnr.nextInt();
-    numPenguins = scnr.nextInt();
+    public static void averageWeight(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int cheetahPounds = 65;
+        int goatPounds = 75;
+        int penguinPounds = 72;
+        int numCheetahs;
+        int numGoats;
+        int numPenguins;
+        int averageWeight;
+        int sumWeight;
 
-    sumWeight = (cheetahPounds * numCheetahs) + (goatPounds * numGoats) + (penguinPounds * numPenguins);
-    averageWeight = sumWeight / (numCheetahs + numGoats + numPenguins);
+        numCheetahs = scnr.nextInt();
+        numGoats = scnr.nextInt();
+        numPenguins = scnr.nextInt();
 
-    System.out.println("Average weight: " + averageWeight);
-  }
+        sumWeight = (cheetahPounds * numCheetahs) + (goatPounds * numGoats) + (penguinPounds * numPenguins);
+        averageWeight = sumWeight / (numCheetahs + numGoats + numPenguins);
 
-  public static void total_heart_beats(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    int userAgeYears;
-    int userAgeDays;
-    int userAgeMinutes;
-    int totalHeartbeats;
-    int avgBeatsPerMinute = 72;
+        System.out.println("Average weight: " + averageWeight);
+    }
 
-    System.out.print("Enter your age in years: ");
-    userAgeYears = scnr.nextInt();
+    public static void total_heart_beats(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int userAgeYears;
+        int userAgeDays;
+        int userAgeMinutes;
+        int totalHeartbeats;
+        int avgBeatsPerMinute = 72;
 
-    userAgeDays = userAgeYears * 365; // Calculate days without leap years
-    userAgeDays = userAgeDays + (userAgeYears / 4); // Add days for leap years
+        System.out.print("Enter your age in years: ");
+        userAgeYears = scnr.nextInt();
 
-    System.out.println("You are " + userAgeDays + " days old.");
+        userAgeDays = userAgeYears * 365; // Calculate days without leap years
+        userAgeDays = userAgeDays + (userAgeYears / 4); // Add days for leap years
 
-    userAgeMinutes = userAgeDays * 24 * 60; // 24 hours/day, 60 minutes/hour
-    System.out.println("You are " + userAgeMinutes + " minutes old.");
+        System.out.println("You are " + userAgeDays + " days old.");
 
-    totalHeartbeats = userAgeMinutes * avgBeatsPerMinute;
-    System.out.println("Your heart has beat " + totalHeartbeats + " times.");
-  }
+        userAgeMinutes = userAgeDays * 24 * 60; // 24 hours/day, 60 minutes/hour
+        System.out.println("You are " + userAgeMinutes + " minutes old.");
 
-  public static void sound_Intensity(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    double soundPower;
-    double distanceTraveled;
+        totalHeartbeats = userAgeMinutes * avgBeatsPerMinute;
+        System.out.println("Your heart has beat " + totalHeartbeats + " times.");
+    }
 
-    soundPower = scnr.nextDouble();
-    distanceTraveled = scnr.nextDouble();
+    public static void sound_Intensity(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double soundPower;
+        double distanceTraveled;
 
-    double soundIntensity = soundPower / (4.0 * Math.PI * (distanceTraveled * distanceTraveled));
+        soundPower = scnr.nextDouble();
+        distanceTraveled = scnr.nextDouble();
 
-    System.out.printf("Sound wave intensity is %.3f\n", soundIntensity);
+        double soundIntensity = soundPower / (4.0 * Math.PI * (distanceTraveled * distanceTraveled));
 
-  }
+        System.out.printf("Sound wave intensity is %.3f\n", soundIntensity);
 
-  public static void gravity_calculation(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    double G = 6.673e-11;
-    double M = 5.98e24;
-    double accelGravity;
-    double distCenter;
+    }
 
-    distCenter = scnr.nextDouble();
+    public static void gravity_calculation(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double G = 6.673e-11;
+        double M = 5.98e24;
+        double accelGravity;
+        double distCenter;
 
-    accelGravity = (G * M) / (distCenter * distCenter);
+        distCenter = scnr.nextDouble();
 
-    System.out.println(accelGravity);
-  }
+        accelGravity = (G * M) / (distCenter * distCenter);
 
-  public static void math_methods(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    double p;
-    double q;
-    double r;
+        System.out.println(accelGravity);
+    }
 
-    p = scnr.nextDouble();
-    q = scnr.nextDouble();
+    public static void math_methods(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double p;
+        double q;
+        double r;
 
-    r = Math.sqrt(Math.abs(p) + Math.abs(q));
+        p = scnr.nextDouble();
+        q = scnr.nextDouble();
 
-    System.out.printf("r = %.2f\n", r); // Outputs r with 2 decimal places
-  }
+        r = Math.sqrt(Math.abs(p) + Math.abs(q));
 
-  public static void math_operators(String[] args) {
-    Scanner scnr = new Scanner(System.in);
-    int totalYears;
-    int numMillennia;
-    int numCenturies;
-    int numYears;
+        System.out.printf("r = %.2f\n", r); // Outputs r with 2 decimal places
+    }
 
-    totalYears = scnr.nextInt();
+    public static void math_operators(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int totalYears;
+        int numMillennia;
+        int numCenturies;
+        int numYears;
 
-    numMillennia = totalYears / 1000;
-    numCenturies = (totalYears / 100) % 10;
-    numYears = totalYears % 100;
+        totalYears = scnr.nextInt();
 
-    System.out.println("Millennia: " + numMillennia);
-    System.out.println("Centuries: " + numCenturies);
-    System.out.println("Years: " + numYears);
-  }
+        numMillennia = totalYears / 1000;
+        numCenturies = (totalYears / 100) % 10;
+        numYears = totalYears % 100;
+
+        System.out.println("Millennia: " + numMillennia);
+        System.out.println("Centuries: " + numCenturies);
+        System.out.println("Years: " + numYears);
+    }
+
+    public static void average_family_size(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int numChildrenA;
+        int numChildrenB;
+        int numFamilies;
+        double avgFamilies;
+
+        numChildrenA = scnr.nextInt();
+        numChildrenB = scnr.nextInt();
+        numFamilies = scnr.nextInt();
+
+        avgFamilies = ((double) (numChildrenA) + (numChildrenB)) / numFamilies;
+
+        System.out.printf("%.2f\n", avgFamilies);
+
+    }
 }
