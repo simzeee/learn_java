379c247659c495cae0b3b5cf6bec26e131fea95a
simzeee <johnsims.dev@gmail.com>
2025-06-23 16:59:14 -0400

    add first java file

---
+++ CS_110/Salary.java
@@ -0,0 +1,10 @@
+public class Salary {                         
+   public static void main (String [] args) { 
+      int wage;                          
+
+      wage = 35;
+
+      System.out.print("Salary is ");         
+      System.out.println(wage * 40 * 52);     
+   } 
+} 
