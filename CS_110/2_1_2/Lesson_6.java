ee2d48e25ade23560da7a8d7161bf2fa972c0cf9
simzeee <jwsims816@gmail.com>
2025-07-06 17:20:57 -0400

    add lesson_6 and first commit

---
+++ CS_110/2_1_2/Lesson_6.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Lesson_6 {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int numElems;
+        int[] studentNumbers;
+        int i;
+        boolean firstSmaller;
+
+        numElems = scnr.nextInt();
+
+        studentNumbers = new int[numElems];
+        for (i = 0; i < studentNumbers.length; ++i) {
+            studentNumbers[i] = scnr.nextInt();
+        }
+
+        if (studentNumbers[0] < studentNumbers[studentNumbers.length - 1]) {
+            firstSmaller = true;
+        } else {
+            firstSmaller = false;
+        }
+
+        if (firstSmaller) {
+            System.out.println("First element is less than last element");
+        } else {
+            System.out.println("First element is not less than last element");
+        }
+    }
+}
