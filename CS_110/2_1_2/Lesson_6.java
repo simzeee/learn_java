c533c56b6144b292cbee7bf8a12a5cda1c7873f5
simzeee <jwsims816@gmail.com>
2025-07-06 17:23:54 -0400

    add numInputArray

--- CS_110/2_1_2/Lesson_6.java
+++ CS_110/2_1_2/Lesson_6.java
@@ -29,4 +29,24 @@ public class Lesson_6 {
             System.out.println("First element is not less than last element");
         }
     }
+
+    public static void numInputArray(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int numElems;
+        int i;
+
+        numElems = scnr.nextInt();
+        int[] patientNumbers = new int[numElems];
+        int numInput;
+        for (i = 0; i < patientNumbers.length; ++i) {
+            numInput = scnr.nextInt();
+            patientNumbers[i] = numInput;
+        }
+
+        System.out.print("Array contents: ");
+        for (i = 0; i < patientNumbers.length; ++i) {
+            System.out.print(patientNumbers[i] + " ");
+        }
+        System.out.println();
+    }
 }
