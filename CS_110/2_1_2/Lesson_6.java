07a65df7c202574c8501539192d60b0b54b64911
simzeee <jwsims816@gmail.com>
2025-07-11 21:11:07 -0400

    finish 6.5

--- CS_110/2_1_2/Lesson_6.java
+++ CS_110/2_1_2/Lesson_6.java
@@ -51,23 +51,79 @@ public class Lesson_6 {
     }
 
     public static void assignArrayValues(String[] args) {
-      Scanner scnr = new Scanner(System.in);
-      final int NUM_VALS = 5;
-		int[] totalNumbersList = new int[NUM_VALS];
-		int i;
+        Scanner scnr = new Scanner(System.in);
+        final int NUM_VALS = 5;
+        int[] totalNumbersList = new int[NUM_VALS];
+        int i;
 
-		for (i = 0; i < totalNumbersList.length; ++i) {
-			totalNumbersList[i] = scnr.nextInt();
-		}
+        for (i = 0; i < totalNumbersList.length; ++i) {
+            totalNumbersList[i] = scnr.nextInt();
+        }
 
-      int[] fourthNumbersList = new int[NUM_VALS];
-      for(i = 0; i < fourthNumbersList.length; ++i){
-         fourthNumbersList[i] = totalNumbersList[i]/4;
-      }
+        int[] fourthNumbersList = new int[NUM_VALS];
+        for (i = 0; i < fourthNumbersList.length; ++i) {
+            fourthNumbersList[i] = totalNumbersList[i] / 4;
+        }
 
-      for (i = 0; i < fourthNumbersList.length; ++i) {
-			System.out.print(fourthNumbersList[i] + " ");
-		}
-		System.out.println();
-   }
+        for (i = 0; i < fourthNumbersList.length; ++i) {
+            System.out.print(fourthNumbersList[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void compareAndAssignArrayValues(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int NUM_VALS = 4;
+        int[] valuesA = new int[NUM_VALS];
+        int[] valuesB = new int[NUM_VALS];
+        int[] largerValues = new int[NUM_VALS];
+        int i;
+
+        for (i = 0; i < valuesA.length; ++i) {
+            valuesA[i] = scnr.nextInt();
+        }
+
+        for (i = 0; i < valuesB.length; ++i) {
+            valuesB[i] = scnr.nextInt();
+        }
+
+        for (i = 0; i < valuesA.length; ++i) {
+            if (valuesA[i] > valuesB[i]) {
+                largerValues[i] = valuesA[i];
+            } else {
+                largerValues[i] = valuesB[i];
+            }
+        }
+
+        for (i = 0; i < largerValues.length; ++i) {
+            System.out.print(largerValues[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int NUM_VALS = 5;
+        int[] groceryItemPrices = new int[NUM_VALS];
+        int[] quantityOrdered = new int[NUM_VALS];
+        int i;
+
+        for (i = 0; i < groceryItemPrices.length; ++i) {
+            groceryItemPrices[i] = scnr.nextInt();
+        }
+
+        for (i = 0; i < quantityOrdered.length; ++i) {
+            quantityOrdered[i] = scnr.nextInt();
+        }
+
+        for (i = 0; i < quantityOrdered.length; ++i) {
+
+            if (quantityOrdered[i] % 2 == 0) {
+                System.out.print(groceryItemPrices[i] + " ");
+            }
+
+        }
+        System.out.println();
+
+    }
 }
