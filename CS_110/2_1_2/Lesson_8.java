ea5c43769dc9b91659fda0593c678ddf0d1e3b2f
simzeee <jwsims816@gmail.com>
2025-07-13 20:27:41 -0400

    add FishermanLicense

---
+++ CS_110/2_1_2/Lesson_8.java
@@ -0,0 +1,25 @@
+
+public class Lesson_8 {
+
+    public class FishermanLicense {
+
+        private int year;
+        private int id;
+
+        public void setYear(int inputYear) {
+            year = inputYear;
+        }
+
+        public void setId(int inputId) {
+            id = inputId;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public int getId() {
+            return id;
+        }
+    }
+}
